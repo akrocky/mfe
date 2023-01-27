60cf27f41b60fd228ea83cbb4fa485c4dba3d14d
akrocky <abul64604@gmail.com>
2023-01-27 12:19:34 +0100

    update

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const {merge} = require('webpack-merge');
 const commonConfig= require('./webpack.common');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin= require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const devConfig={
@@ -21,9 +21,7 @@ const devConfig={
         shared:packageJson.dependencies,
        })
         ,
-        new HtmlWebpackPlugin({
-            template:'./public/index.html',
-        })
+       
     ]
 
 };
